7698510d3e651154a721dc947884f2ada5400bc3
Luna160-star <rakhigoalsachieve@gmail.com>
2025-10-26 00:00:32 +0530

    trying to make work the progress bar

---
+++ loading.js
@@ -0,0 +1,8 @@
+const loadingBar = document.getElementByClassName ('loading-bar')[0]
+setInterval(() => {
+    const computedStyle = getComputedStyle(loadingBar)
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+
+    loadingBar.style.setProperty('--width', width + .1)
+}, 5)
+
