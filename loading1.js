5f71b1ed3d84186d7c88d9641faae80dd9b99dcb
Luna160-star <rakhigoalsachieve@gmail.com>
2025-10-28 15:57:50 +0530

    correction done

---
+++ loading1.js
@@ -0,0 +1,11 @@
+function goToLoading1() {
+            setTimeout(() =>{
+            window.location.href = "loading page1.html";
+        }, 1000);
+}
+
+function goToLoading2() {
+            setTimeout(() =>{
+            window.location.href = "loading2.html";
+        }, 1000);
+}
